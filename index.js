dd92c59a2f0f2431c49dd291a441576b22f91d71
Deryn Brown <Deryn.2017@gmail.com>
2022-09-06 21:46:11 -0500

    commit tree 733eceb032413da3a428d98d0e359eec4153be4e

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function (object){
+        document.body.innerHTML = object["id"]
+    })
+    .catch(function (error) {
+        document.body.innerHTML = error.message
+    })
+}
+//     const formData = {
+//         name: userName,
+//         email: userEmail,
+//     };
+
+//     const configurationObject = {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json",
+//             },
+//             body: JSON.stringify(formData)
+//         };
+
+//     return fetch("http://localhost:3000/users", configurationObject)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(object){
+//         console.log(object);
+//     })
+//     .catch(function(error) {
+//         console.log(error.message);
+//     });
+// }
+
+//     fetch("http://localhost:3000/users", configurationObject)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (object) {
+//         console.log(object);
+//     }),
+
+
+// function submitData(name, email) {
+//     method: "POST",
+
+//     fetch("http://localhost:3000/users", {
+
+//     }),
+//     .then(),
+//     .then();
+//     },
